a02667c9798700b4cff0582b56832cd0dca376d2
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-21 20:08:58 -0500

    tried putting the URL into a variable to call in a fetch

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 window.addEventListener('load', function() {
     console.log ('window has loaded');
-    const urlEntry = document.getElementById
-    let fetchPromise =fetch('http://api.openweathermap.org/data/2.5/weather?zip=63385&appid=e770ede1e51fbbb66b830c2986e6bb7a');
-    fetchPromise.then(function(response){
-        const jsonPromise = response.json();
-        jsonPromise.then(function(json){
-            console.log(json);
-            let htmlResult = document.getElementById('container');
-            for (key in json){
-                htmlResult.innerHTML+= `<div>${key}</div>`;
-            }
-        });
-    });  
+    let htmlResult= document.getElementById('container');
+    let apiToTry="";
+    let submitButton = document.getElementById("submitUrlButton");
+    let urlToTest = "";
+    submitButton.addEventListener("click", function(event){
+        event.preventDefault();
+        logUrl();
+        let responsePromise = fetch(urlToTest);
+        responsePromise.then(function(response){
+                console.log ('fetch fired: ' +urlToTest);
+        })
+    })
 });
 
+
+function logUrl (){
+    urlToTest = document.getElementById("apiurl");
+    urlToTest = String(urlToTest.value);
+    console.log (urlToTest);
+
+};
